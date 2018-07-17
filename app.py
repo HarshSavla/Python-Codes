d2a776832f5c39a052ae6814d3392fdfd9bdecdc
HarshSavla <harsh.savla007@gmail.com>
2018-07-17 16:02:47 +0530

    FIRST

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ def homepage():
 
     return "<h1>Hello heroku</h1><p>It is currently {time}.</p>"
 
+@app.route('/contact')
+def contact():
+
+    return "cont num; 1234, EMILID;fere"
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
     
