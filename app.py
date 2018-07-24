94457eb637d3eda51f0222345fff1c31909154be
HarshSavla <harsh.savla007@gmail.com>
2018-07-24 15:02:25 +0530

    first

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
 
-    return render_template('today.html')
+    return render_template('Today.html')
 
 @app.route('/contact')
 def contact():
