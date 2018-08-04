fa701a86fda406bf7584408ac1b65749737098d4
HarshSavla <harsh.savla007@gmail.com>
2018-08-04 22:07:44 +0530

    first

--- app.py
+++ app.py
@@ -32,14 +32,14 @@ def trackPerson(device_id, person_id):
     ban = os.environ.get(g.bandname)
     location.append([loc])
     bandname.append([ban])
-    length = len(location) 
+    length = [len(location)] 
 
     return "OK"
 
 @app.route('/')
 def homepage():
 
-    return render_template('Today.html', location=location, bandname=bandname, length=length, L=L)
+    return render_template('Today.html', location=location, bandname=bandname, length=length[0], L=L)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
