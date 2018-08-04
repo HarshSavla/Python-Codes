c82ac8ce0b7255d7179617ec7b8c91612ab145e4
HarshSavla <harsh.savla007@gmail.com>
2018-08-04 14:51:04 +0530

    first

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def trackPerson(device_id, person_id):
     g.bandname = person_id
     location.append(os.environ.get(g.location))
     bandname.append(os.environ.get(g.bandname))
-    return "OK"
+    return render_template('Today.html', location=location, bandname=bandname)
 
 @app.route('/')
 def homepage():
